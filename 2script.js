9c3ff837db0c96f75ff1677d75a8c8c52289248b
SM-Smart-SK <simco2842@gmail.com>
2026-03-10 16:23:12 +0100

    Update 2script.js

--- 2script.js
+++ 2script.js
@@ -1,1 +1,43 @@
+const key = {
+    'A': 'B', 'B': 'C', 'C': 'D', 'D': 'E', 'E': 'F', 'F': 'G', 
+    'G': 'H', 'H': 'I', 'I': 'J', 'J': 'K', 'K': 'L', 'L': 'M', 
+    'M': 'N', 'N': 'O', 'O': 'P', 'P': 'Q', 'Q': 'R', 'R': 'S', 
+    'S': 'T', 'T': 'U', 'U': 'V', 'V': 'W', 'W': 'X', 'X': 'Y', 
+    'Y': 'Z', 'Z': 'A',
+    '1': '2', '2': '3', '3': '4', '4': '5', '5': '6', '6': '7', 
+    '7': '8', '8': '9', '9': '0', '0': '1',
+    '.': '+', ',': '=', '!': '?', '?': '_', ':': '/', ';': '|'
+};
 
+const reverseKey = Object.fromEntries(Object.entries(key).map(([k, v]) => [v, k]));
+
+function encrypt() {
+    let input = document.getElementById('inputText').value;
+    input = input.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase();
+    if(!input) return;
+
+    const words = input.trim().split(/\s+/);
+    const result = words.map(word => {
+        return word.split('').map(char => key[char] || char).join('-');
+    }).join('--');
+
+    document.getElementById('outputText').innerText = result;
+}
+
+function decrypt() {
+    const input = document.getElementById('inputText').value.trim();
+    if (!input) return;
+
+    const words = input.split('--');
+    const result = words.map(word => {
+        return word.split('-').map(char => reverseKey[char] || char).join('');
+    }).join(' ');
+
+    document.getElementById('outputText').innerText = result;
+}
+
+function copyToClipboard() {
+    const text = document.getElementById('outputText').innerText;
+    navigator.clipboard.writeText(text);
+    alert("Skopírované!");
+}
